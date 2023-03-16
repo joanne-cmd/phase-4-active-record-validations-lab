357c7a3462b8be9b3011d5454f0135b5d54ca983
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-16 04:49:28 +0300

    finished

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in:["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+
+  def title_is_clickbait
+    if title.present? && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+      errors.add(:title, "must be clickbait-y!")
+    end
+end
 end
